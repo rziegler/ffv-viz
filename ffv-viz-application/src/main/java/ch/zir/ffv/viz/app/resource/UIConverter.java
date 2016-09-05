fb4a42ebb7b5644463b937e3da6cbb2b806efb4b
ueli <ueli.kurmann@gmail.com>
2016-09-05 17:30:44 +0200

    Korrektur Buckets

--- ffv-viz-application/src/main/java/ch/zir/ffv/viz/app/resource/UIConverter.java
+++ ffv-viz-application/src/main/java/ch/zir/ffv/viz/app/resource/UIConverter.java
@@ -16,7 +16,7 @@ public class UIConverter {
 		
 		for(FlightInformation flight:flights){
 			double diff = flight.getMaxPrice()-flight.getMinPrice();
-			double slice = diff / 7.0;
+			double slice = diff / 6.0;
 			for(Price price:flight.getPrices()){
 				UIFlight f = new UIFlight();
 				f.setBin((int)((price.getPrice() - flight.getMinPrice()) / slice));
